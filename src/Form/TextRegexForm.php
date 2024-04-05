5d2e112173b6e9d8e644f3c3d95e61c4eed6f378
vynograd92@gmail.com <vynograd92@gmail.com>
2024-04-05 23:11:37 +0300

    finish test work

--- src/Form/TextRegexForm.php
+++ src/Form/TextRegexForm.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use App\Service\Delivery\TransportService\PackGroupTransport;
-use App\Service\Delivery\TransportService\TransCompanyTransport;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class TextRegexForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
--- src/Service/Delivery/TransportService/TransportInterface.php
+++ src/Service/Delivery/TransportService/TransportInterface.php
@@ -6,5 +6,6 @@ namespace App\Service\Delivery\TransportService;
 interface TransportInterface
 {
     public function calculatePrice(float $weight): float;
+
     public function getSlug(): string;
 }
